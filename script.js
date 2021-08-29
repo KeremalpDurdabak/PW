23996f37f358c35c713ba576f2e9faca4ef366de
KeremalpDurdabak <keremalp0@hotmail.com>
2021-08-29 14:03:49 +0300

    Website Update 2021

---
+++ script.js
@@ -0,0 +1,39 @@
+let colors = ["#00b4d8", "#ffbe0b", "#12b35a", "#8338ec", "#3a86ff"];
+
+(function() {
+  setBioEventListener();
+})();
+
+/* Colors */
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+/* Bio Toggles */
+
+function setBioEventListener() {
+  Array.from(document.getElementsByTagName("button")).forEach(e => {
+    e.addEventListener("click", bioToggle);
+  });
+}
+
+function bioToggle(e) {
+  let bioType = e.target;
+  let color = getRandomColor();
+  off(bioType);
+  bioType.style.cssText = `border-color: ${color}; color: ${color}; font-weight: bold;`;
+  let bioTypeElement = document.getElementsByClassName(bioType.id)[0];
+  if (bioTypeElement !== undefined) bioTypeElement.classList.add("show");
+}
+
+function off(bioType) {
+  Array.from(document.getElementsByTagName("button")).forEach(btn => {
+    btn.style.borderColor = "#96979c";
+    btn.style.color = "#96979c";
+    btn.style.fontWeight = "normal";
+  });
+  Array.from(document.getElementsByClassName("bio")).forEach(e => {
+    e.classList.remove("show");
+  });
+}
